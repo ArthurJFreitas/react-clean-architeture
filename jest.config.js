27b83963e695c729ffa3bf3bb5a99f88c5f5df97
Arthur <arthu_junior@hotmail.com>
2022-03-07 00:35:31 -0300

    feat: add authentication usecase

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  // The root of your source code, typically /src
+  // `<rootDir>` is a token Jest substitutes
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx}'
+  ],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
